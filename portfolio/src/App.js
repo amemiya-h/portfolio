cac4743a84cd4a991a5f74ed88c02c3e4f86bfa4
amemiya-h <aozora.kei.works@gmail.com>
2024-08-16 15:49:16 +0900

    Update

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import logo from './logo-color.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+      <div className='content'>
+        <img src={logo} className="logo" alt="logo" />
+        <div className='textbox'>
+          <p className='text'>
+            This website is currently under maintenance.
+          </p>
+          <p className='text'>
+            本ウェブサイトはただいま準備中です。
+          </p>
+        </div>
+      </div>
+      <div className='footer'>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        雨宮葉流 © 2024 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
